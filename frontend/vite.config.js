7278b625dd04718d41d7d3f2afaadf0416bbfa4c
jkazimierczak <77862767+jkazimierczak@users.noreply.github.com>
2024-06-07 00:59:47 +0200

    build: add vite

---
+++ frontend/vite.config.js
@@ -0,0 +1,13 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react({
+    jsxRuntime: 'classic',
+  })],
+  server: {
+    host: process.env.HOST || '0.0.0.0',
+    port: process.env.PORT || 8000,
+  },
+});
