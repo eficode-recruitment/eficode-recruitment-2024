df02c697d36cc57cc200e5164db771f0e4632024
jkazimierczak <77862767+jkazimierczak@users.noreply.github.com>
2024-06-08 00:24:04 +0200

    feat: provision e2-micro vm instance

--- main.tf
+++ main.tf
@@ -17,3 +17,22 @@ resource "google_compute_network" "vpc_network" {
   name = "terraform-network"
 }
 
+resource "google_compute_instance" "vm_instance" {
+  name         = "eficode-vm"
+  machine_type = "e2-micro"
+
+  boot_disk {
+    initialize_params {
+      image = "ubuntu-os-cloud/ubuntu-2404-lts-amd64"
+    }
+  }
+
+  network_interface {
+    network = google_compute_network.vpc_network.name
+
+    access_config {
+      // Give the VM an external IP address
+    }
+  }
+}
+
