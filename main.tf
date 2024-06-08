42f48b78c613989e1c742630e2308997aed2ccaa
jkazimierczak <77862767+jkazimierczak@users.noreply.github.com>
2024-06-08 03:08:36 +0200

    feat: output nat_ip of created instance

--- main.tf
+++ main.tf
@@ -56,3 +56,7 @@ resource "google_compute_firewall" "firewall-allow-ssh" {
   target_tags   = ["ssh"]
 }
 
+output "nat_ip" {
+  description = "The public IP of the instance"
+  value = google_compute_instance.vm_instance.network_interface.0.access_config.0.nat_ip
+}
