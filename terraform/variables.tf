3558e9430ac666fae59322833b0d3ee65ce17ada
jkazimierczak <77862767+jkazimierczak@users.noreply.github.com>
2024-06-10 04:42:20 +0200

    refactor: extract vm definition into separate file

--- terraform/variables.tf
+++
@@ -1,7 +0,0 @@
-variable "gce_vm_type" {}
-
-variable "gce_ssh_user" {}
-variable "gce_ssh_user_eficode" {}
-
-variable "gce_ssh_pub_key" {}
-variable "gce_ssh_pub_key_eficode" {}
---
+++ terraform/vm-variables.tf
@@ -0,0 +1,17 @@
+variable "gce_vm_type" {
+  description = "The type of the VM to create"
+}
+
+variable "gce_ssh_user" {
+  description = "The SSH username to create on the VM"
+}
+variable "gce_ssh_user_eficode" {
+  description = "The SSH username to create on the VM"
+}
+
+variable "gce_ssh_pub_key" {
+  description = "The SSH public key to add to the VM"
+}
+variable "gce_ssh_pub_key_eficode" {
+  description = "The SSH public key to add to the VM"
+}
